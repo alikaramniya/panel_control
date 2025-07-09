891131ce9cee3df2b555680d9d86468946cdf63d
alikaram <alikaramniyabadreh98@gmail.com>
2025-07-09 10:09:10 +0330

    Establishing a relationship between the user model and documents

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -4,9 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Document extends Model
 {
     /** @use HasFactory<\Database\Factories\DocumentFactory> */
     use HasFactory;
+
+    protected $table = 'documents';
+
+    protected $fillable = [
+        'user_id', 'file_type', 
+        'file', 'file_name', 
+        'file_date_upload'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
